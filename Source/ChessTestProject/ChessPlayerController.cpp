3df05b40501fb5340d4562d7f445d1d90a371d22
Igor Tytarenko <i.tytarenko@solidcore.games>
2021-06-20 13:34:33 +0300

    Add Player controller

---
+++ Source/ChessTestProject/ChessPlayerController.cpp
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChessPlayerController.h"
+#include "ChessTestProjectGameModeBase.h"
+#include "ChessBoard.h"
+#include "Kismet/GameplayStatics.h"
+
+void AChessPlayerController::BeginPlay()
+{
+	if (auto world = GetWorld())
+	{
+		TArray<AActor*> foundActors;
+		UGameplayStatics::GetAllActorsOfClass(world, AChessBoard::StaticClass(), foundActors);
+
+		if (foundActors.Num() > 0)
+		{
+			SetViewTarget(foundActors[0]);
+		}
+	}
+
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
+}
+
+void AChessPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindKey(EKeys::M, IE_Released, this, &AChessPlayerController::rotateCamera);
+	InputComponent->BindKey(EKeys::LeftMouseButton, IE_Pressed, this, &AChessPlayerController::ProcessMouseClick);
+}
+
+void AChessPlayerController::rotateCamera()
+{
+	if (auto gameMode = Cast<AChessTestProjectGameModeBase>(GetWorld()->GetAuthGameMode()))
+	{
+		gameMode->EndCurrentPlayerTurn();
+	}
+}
+
+void AChessPlayerController::ProcessMouseClick()
+{
+	// Trace to see what is under the mouse cursor
+	FHitResult hit;
+	GetHitResultUnderCursor(ECC_Visibility, true, hit);
+
+	if (hit.bBlockingHit)
+	{
+		auto actorHit = hit.Actor;
+		if (auto chessPiece = Cast<AChessPiece>(actorHit.Get()))
+		{
+			if (mSelectedPiece && mSelectedPiece != chessPiece)
+			{
+				mSelectedPiece->setSelected(false);
+			}
+			mSelectedPiece = chessPiece;
+			mSelectedPiece->setSelected(true);
+
+			if (auto chessMode = Cast<AChessTestProjectGameModeBase>(GetWorld()->GetAuthGameMode()))
+			{
+				chessMode->ShowPiecePossibleMovement(mSelectedPiece);
+			}
+
+			UE_LOG(LogTemp, Warning, TEXT("Selected : %s"), *chessPiece->GetName());
+		}
+		else if (auto chessBoard = Cast<AChessBoard>(actorHit.Get()))
+		{
+			auto component = hit.Component;
+			if (component.IsValid()) // to be on safe side
+			{
+				// if it is the selector
+				if (auto parent = Cast<UStaticMeshComponent>(component->GetAttachParent()))
+				{
+					component = parent;
+				}
+
+				if (component.IsValid() && mSelectedPiece)
+				{
+					if (auto chessMode = Cast<AChessTestProjectGameModeBase>(GetWorld()->GetAuthGameMode()))
+					{
+						if (chessMode->playTurn(mSelectedPiece, Cast<UStaticMeshComponent>(component.Get())))
+						{
+							mSelectedPiece->setSelected(false);
+							mSelectedPiece = nullptr;
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Source/ChessTestProject/ChessPlayerController.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ChessPiece.h"
+#include "ChessPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESSTESTPROJECT_API AChessPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BeginPlay() override;
+	void SetupInputComponent() override;
+
+private:
+	void rotateCamera();
+	void ProcessMouseClick();
+
+	AChessPiece* mSelectedPiece = nullptr;
+};
